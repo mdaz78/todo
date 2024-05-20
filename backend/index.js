702bf3eb2905a7d71622d6951ff4d29441f4aa16
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-20 11:10:45 +0530

    feat: handle cors errors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const { createTodo } = require("./types");
 const { todo } = require("./db");
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.post("/todo", async (req, res) => {
   const createPayload = req.body;
