fc352a3e158045a7fbcb3a4ff6490d43ddd80414
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-20 11:27:43 +0530

    feat: Add functionality to create and mark todos as done

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,4 +1,17 @@
+import axios from "axios";
+import { useState } from "react";
+
 const CreateTodo = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleClick = async () => {
+    await axios.post("http://localhost:3000/todo", {
+      title,
+      description,
+    });
+  };
+
   return (
     <div className="grid grid-cols-3">
       <input
@@ -7,6 +20,8 @@ const CreateTodo = () => {
         id=""
         placeholder="title"
         className="border-2"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <input
         type="text"
@@ -14,9 +29,14 @@ const CreateTodo = () => {
         id=""
         placeholder="description"
         className="border-2"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
 
-      <button className="btn p-2 rounded-md bg-slate-100 hover:bg-slate-200">
+      <button
+        className="btn p-2 rounded-md bg-slate-100 hover:bg-slate-200"
+        onClick={handleClick}
+      >
         Add Todo
       </button>
     </div>
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,11 +1,30 @@
+import axios from "axios";
+
 /* eslint-disable react/prop-types */
 const Todos = ({ todos }) => {
+  const handleClick = async (id) => {
+    await axios.put("http://localhost:3000/completed", {
+      id,
+    });
+  };
+
   return (
-    <div>
-      {todos.map(({ title, description }) => (
-        <div key={title}>
+    <div className="flex space-x-2 space-y-2">
+      {todos.map(({ _id: id, title, description, completed }) => (
+        <div
+          key={id}
+          className={`border-2 border-red-300 rounded-md p-2 ${
+            completed ? "opacity-30" : ""
+          }`}
+        >
           <h1>{title}</h1>
           <p>{description}</p>
+          <button
+            className="bg-slate-300 hover:bg-slate-200 rounded-md px-2 py-1"
+            onClick={() => handleClick(id)}
+          >
+            Mark as Done
+          </button>
         </div>
       ))}
     </div>
